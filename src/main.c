5bae09cffa62fd15ac595eb46fe8981308c216bb
quynhanh0107 <qanhhle010706@gmail.com>
2025-11-06 11:38:26 +0200

    datatest

--- src/main.c
+++ src/main.c
@@ -10,202 +10,282 @@
 
 #include "tkjhat/sdk.h"
 
-// Exercise 4. Include the libraries necessaries to use the usb-serial-debug, and tinyusb
-// Tehtävä 4 . Lisää usb-serial-debugin ja tinyusbin käyttämiseen tarvittavat kirjastot.
+#include <hardware/gpio.h>
+#include <hardware/i2c.h>
 
 
 
-#define DEFAULT_STACK_SIZE 2048
-#define CDC_ITF_TX      1
+#define BUFFER_SIZE 40
+
+#define TILT_THRESHOLD 
+#define FLAT_THRESHOLD 
+#define SHAKE_THRESHOLD 
 
 
-// Tehtävä 3: Tilakoneen esittely Add missing states.
-// Exercise 3: Definition of the state machine. Add missing states.
-enum state { WAITING=1};
-enum state programState = WAITING;
+typedef enum IMU_states {
+    STATE_IDLE = 1,    // In-between positions
+    STATE_DOT,     // Being shaken 
+    STATE_DASH,    // Tilted 90 degrees
+    STATE_SPACE    // Flat on table
+} imu_event;
 
-// Tehtävä 3: Valoisuuden globaali muuttuja
-// Exercise 3: Global variable for ambient light
-uint32_t ambientLight;
-
-static void btn_fxn(uint gpio, uint32_t eventMask) {
-    // Tehtävä 1: Vaihda LEDin tila.
-    //            Tarkista SDK, ja jos et löydä vastaavaa funktiota, sinun täytyy toteuttaa se itse.
-    // Exercise 1: Toggle the LED. 
-    //             Check the SDK and if you do not find a function you would need to implement it yourself. 
-}
-
-static void sensor_task(void *arg){
-    (void)arg;
-    // Tehtävä 2: Alusta valoisuusanturi. Etsi SDK-dokumentaatiosta sopiva funktio.
-    // Exercise 2: Init the light sensor. Find in the SDK documentation the adequate function.
-   
-    for(;;){
-        
-        // Tehtävä 2: Muokkaa tästä eteenpäin sovelluskoodilla. Kommentoi seuraava rivi.
-        //             
-        // Exercise 2: Modify with application code here. Comment following line.
-        //             Read sensor data and print it out as string; 
-        tight_loop_contents(); 
-
-
-   
-
-
-        // Tehtävä 3:  Muokkaa aiemmin Tehtävässä 2 tehtyä koodia ylempänä.
-        //             Jos olet oikeassa tilassa, tallenna anturin arvo tulostamisen sijaan
-        //             globaaliin muuttujaan.
-        //             Sen jälkeen muuta tilaa.
-        // Exercise 3: Modify previous code done for Exercise 2, in previous lines. 
-        //             If you are in adequate state, instead of printing save the sensor value 
-        //             into the global variable.
-        //             After that, modify state
-
-
-
-
-
-        
-        // Exercise 2. Just for sanity check. Please, comment this out
-        // Tehtävä 2: Just for sanity check. Please, comment this out
-        printf("sensorTask\n");
-
-        // Do not remove this
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
-
-static void print_task(void *arg){
-    (void)arg;
-    
-    while(1){
-        
-        // Tehtävä 3: Kun tila on oikea, tulosta sensoridata merkkijonossa debug-ikkunaan
-        //            Muista tilamuutos
-        //            Älä unohda kommentoida seuraavaa koodiriviä.
-        // Exercise 3: Print out sensor data as string to debug window if the state is correct
-        //             Remember to modify state
-        //             Do not forget to comment next line of code.
-        tight_loop_contents();
-        
-
-
-        
-        // Exercise 4. Use the usb_serial_print() instead of printf or similar in the previous line.
-        //             Check the rest of the code that you do not have printf (substitute them by usb_serial_print())
-        //             Use the TinyUSB library to send data through the other serial port (CDC 1).
-        //             You can use the functions at https://github.com/hathach/tinyusb/blob/master/src/class/cdc/cdc_device.h
-        //             You can find an example at hello_dual_cdc
-        //             The data written using this should be provided using csv
-        //             timestamp, luminance
-        // Tehtävä 4. Käytä usb_serial_print()-funktiota printf:n tai vastaavien sijaan edellisellä rivillä.
-        //            Tarkista myös muu koodi ja varmista, ettei siinä ole printf-kutsuja
-        //            (korvaa ne usb_serial_print()-funktiolla).
-        //            Käytä TinyUSB-kirjastoa datan lähettämiseen toisen sarjaportin (CDC 1) kautta.
-        //            Voit käyttää funktioita: https://github.com/hathach/tinyusb/blob/master/src/class/cdc/cdc_device.h
-        //            Esimerkki löytyy hello_dual_cdc-projektista.
-        //            Tällä menetelmällä kirjoitettu data tulee antaa CSV-muodossa:
-        //            timestamp, luminance
-
-
-
-
-        // Exercise 3. Just for sanity check. Please, comment this out
-        // Tehtävä 3: Just for sanity check. Please, comment this out
-        printf("printTask\n");
-        
-        // Do not remove this
-        vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
-
-
-// Exercise 4: Uncomment the following line to activate the TinyUSB library.  
-// Tehtävä 4:  Poista seuraavan rivin kommentointi aktivoidaksesi TinyUSB-kirjaston. 
+//static enum IMU_states last_state = STATE_IDLE;
+//static int space_count = 0; 
 
 /*
-static void usbTask(void *arg) {
-    (void)arg;
-    while (1) {
-        tud_task();              // With FreeRTOS wait for events
-                                 // Do not add vTaskDelay. 
+// Introducing state
+enum state {IDLE=1, READ_SENSOR, UPDATE, NEW_MSG};
+
+// Global state variable, initialized to waiting state
+enum state myState = IDLE;
+
+// Timer interrupt once per second
+void clkFxn(void *pvParameters) {
+    // We change the state to a desired one
+    // If-clause is used to check, that the state transition is possible
+    // Now we allow only the state transition IDLE -> READ_SENSOR
+    if (myState == IDLE) {
+    
+        // State transition IDLE -> READ_SENSOR
+        myState = READ_SENSOR;
     }
-}*/
-
-int main() {
-
-    // Exercise 4: Comment the statement stdio_init_all(); 
-    //             Instead, add AT THE END OF MAIN (before vTaskStartScheduler();) adequate statements to enable the TinyUSB library and the usb-serial-debug.
-    //             You can see hello_dual_cdc for help
-    //             In CMakeLists.txt add the cfg-dual-usbcdc
-    //             In CMakeLists.txt deactivate pico_enable_stdio_usb
-    // Tehtävä 4:  Kommentoi lause stdio_init_all();
-    //             Sen sijaan lisää MAIN LOPPUUN (ennen vTaskStartScheduler();) tarvittavat komennot aktivoidaksesi TinyUSB-kirjaston ja usb-serial-debugin.
-    //             Voit katsoa apua esimerkistä hello_dual_cdc.
-    //             Lisää CMakeLists.txt-tiedostoon cfg-dual-usbcdc
-    //             Poista CMakeLists.txt-tiedostosta käytöstä pico_enable_stdio_usb
-
-    stdio_init_all();
-
-    // Uncomment this lines if you want to wait till the serial monitor is connected
-    /*while (!stdio_usb_connected()){
-        sleep_ms(10);
-    }*/ 
-    
-    init_hat_sdk();
-    sleep_ms(300); //Wait some time so initialization of USB and hat is done.
-
-    // Exercise 1: Initialize the button and the led and define an register the corresponding interrupton.
-    //             Interruption handler is defined up as btn_fxn
-    // Tehtävä 1:  Alusta painike ja LEd ja rekisteröi vastaava keskeytys.
-    //             Keskeytyskäsittelijä on määritelty yläpuolella nimellä btn_fxn
-
-
-
-    
-    
-    TaskHandle_t hSensorTask, hPrintTask, hUSB = NULL;
-
-    // Exercise 4: Uncomment this xTaskCreate to create the task that enables dual USB communication.
-    // Tehtävä 4: Poista tämän xTaskCreate-rivin kommentointi luodaksesi tehtävän,
-    // joka mahdollistaa kaksikanavaisen USB-viestinnän.
-
-    /*
-    xTaskCreate(usbTask, "usb", 2048, NULL, 3, &hUSB);
-    #if (configNUMBER_OF_CORES > 1)
-        vTaskCoreAffinitySet(hUSB, 1u << 0);
-    #endif
-    */
-
-
-    // Create the tasks with xTaskCreate
-    BaseType_t result = xTaskCreate(sensor_task, // (en) Task function
-                "sensor",                        // (en) Name of the task 
-                DEFAULT_STACK_SIZE,              // (en) Size of the stack for this task (in words). Generally 1024 or 2048
-                NULL,                            // (en) Arguments of the task 
-                2,                               // (en) Priority of this task
-                &hSensorTask);                   // (en) A handle to control the execution of this task
-
-    if(result != pdPASS) {
-        printf("Sensor task creation failed\n");
-        return 0;
-    }
-    result = xTaskCreate(print_task,  // (en) Task function
-                "print",              // (en) Name of the task 
-                DEFAULT_STACK_SIZE,   // (en) Size of the stack for this task (in words). Generally 1024 or 2048
-                NULL,                 // (en) Arguments of the task 
-                2,                    // (en) Priority of this task
-                &hPrintTask);         // (en) A handle to control the execution of this task
-
-    if(result != pdPASS) {
-        printf("Print Task creation failed\n");
-        return 0;
-    }
-
-    // Start the scheduler (never returns)
-    vTaskStartScheduler();
-    
-    // Never reach this line.
-    return 0;
 }
 
+// Communications task
+void commTask(void *pvParameters) {
+
+    while (1) { 
+        // Function is_message_waiting is used to check
+        // are there new messages in the buffer
+        // Additionally, we allow only the state transition IDLE -> NEW_MSG
+        if (is_message_waiting() == TRUE && myState == IDLE) {
+
+            // State transition IDLE -> NEW_MSG
+            myState = NEW_MSG;
+        
+            // Functionality of state
+            handle_message();
+            send_reply();			        
+            
+            // State transition NEW_MSG -> IDLE
+            myState = IDLE;
+        }
+    }
+}
+
+// Handling sensors
+void sensorTask(void *pvParameters) {
+   
+    //Start the I2C
+    init_i2c_default();
+
+    while (1) {
+    
+        if (myState == READ_SENSOR) {
+        
+            // Functionality of state
+            read_sensor_values();
+            
+            // State transition READ_SENSOR -> UPDATE
+            myState = UPDATE;				        
+        }
+    
+        vTaskDelay(..);
+    }
+}
+
+// Handling display update
+void displayTask(void *pvParameters) {
+
+    while (1) {
+    
+        if (myState == UPDATE) {
+        
+            // Functionality of state
+            update_screen();
+            
+            // State transition UPDATE -> IDLE
+            myState = IDLE;				        
+        }
+    
+        vTaskDelay(..);
+    }
+}
+*/
+
+void imu_task(void *pvParameters) {
+    (void)pvParameters;
+    
+    float ax, ay, az, gx, gy, gz, t;
+    // Setting up the sensor. 
+    if (init_ICM42670() == 0) {
+        printf("ICM-42670P initialized successfully!\n");
+        if (ICM42670_start_with_default_values() != 0){
+            printf("ICM-42670P could not initialize accelerometer or gyroscope");
+        }
+        /*int _enablegyro = ICM42670_enable_accel_gyro_ln_mode();
+        usb_serial_print ("Enable gyro: %d\n",_enablegyro);
+        int _gyro = ICM42670_startGyro(ICM42670_GYRO_ODR_DEFAULT, ICM42670_GYRO_FSR_DEFAULT);
+        usb_serial_print ("Gyro return:  %d\n", _gyro);
+        int _accel = ICM42670_startAccel(ICM42670_ACCEL_ODR_DEFAULT, ICM42670_ACCEL_FSR_DEFAULT);
+        usb_serial_print ("Accel return:  %d\n", _accel);*/
+    } else {
+        printf("Failed to initialize ICM-42670P.\n");
+    }
+    // Start collection data here. Infinite loop. 
+    uint8_t buf[BUFFER_SIZE];
+    while (1)
+    {
+        if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) == 0) {
+
+            /*enum IMU_states current_state = STATE_IDLE;
+
+            //*******Determine the current state based on thresholds*******
+            // STATE_SPACE: flat position
+            if (az > FLAT_THRESHOLD) {
+                current_state = STATE_SPACE;
+
+            }
+            //STATE_DASH: rotating 90 degrees 
+            else if (ay > TILT_THRESHOLD) {
+                current_state = STATE_DASH;
+            }
+            //STATE_DOT: shaking
+            else if (fabs(gx) > SHAKE_THRESHOLD || fabs(gy) > SHAKE_THRESHOLD || fabs(gz) > SHAKE_THRESHOLD ) {
+                current_state = STATE_DOT;
+            }
+
+            //*******Execute only if state changes*******
+            if (current_state != last_state) {
+                switch (current_state) {
+                    case STATE_DOT:
+                        printf(".");
+                        space_count = 0; // reset space count 
+                        break;
+                    case STATE_DASH:
+                        printf("-");
+                        space_count = 0;
+                        break;
+                    case STATE_SPACE: 
+                        printf("");
+                        space_count ++;
+                        break;
+                    case STATE_IDLE:
+                        break;
+
+                }
+                if (space_count >= 3) {
+                    printf("End of message");
+                    space_count = 0;
+                }
+            }
+            last_state = current_state;
+           */
+            printf("Accel: X=%.2f, Y=%.2f, Z=%.2f | Gyro: X=%.2f, Y=%.2f, Z=%.2f \n", ax, ay, az, gx, gy, gz);
+
+        } else {
+            printf("Failed to read imu data\n");
+        }
+        vTaskDelay(pdMS_TO_TICKS(60));
+    }
+
+}
+
+
+
+
+
+int main() {
+    stdio_init_all();
+    sleep_ms(2000); //Wait to see the output.
+    init_hat_sdk();
+    while (!stdio_usb_connected()){
+        sleep_ms(10);
+    } 
+    printf("Start tests\n");
+    
+    // Initialize LED
+    init_red_led();
+    printf("Initializing red led\r\n");
+    
+    //Testing RED LED
+    // printf("Testing red led should be on\r\n");
+   
+
+
+    // Test SW1 
+    //init_sw1();
+    //printf("Initializing switch 1\r\n");
+
+    //Test SW2
+    //init_sw2();
+    //printf("Initializing switch 2\r\n");
+
+    //Test RGB
+    //init_rgb_led();
+    //printf("Initializing RGB LED\r\n");
+
+
+   // Initialize Buzzer
+   //init_buzzer();
+   //printf("Initializing the buzzer\n");
+
+   
+    // Initialize I2C
+    init_i2c_default();
+    printf("Initializing the i2c\n");
+
+    //Initialize Light Sesnsor VEML6030
+    //veml6030_init();
+    //printf("Initializing the light sensor\n");
+
+    //Initialize the Temp and Humidity Sensor
+    //hdc2021_init();
+    //printf("Initializing the temp/humidity sensor\n");
+
+    //Initialize the display
+    //init_display();
+    //printf("Initializating display\n");
+
+    //Initialize the microphone
+    //Microhpone test in test_microphone.c
+
+    //Initialize IMU
+    if (init_ICM42670() == 0) {
+        printf("ICM-42670P initialized successfully!\n");
+        if (ICM42670_startAccel(ICM42670_ACCEL_ODR_DEFAULT, ICM42670_ACCEL_FSR_DEFAULT) != 0){
+            printf("Wrong values to init the accelerometer in ICM-42670P.\n");
+        }
+        if (ICM42670_startGyro(ICM42670_GYRO_ODR_DEFAULT, ICM42670_GYRO_FSR_DEFAULT) != 0){
+            printf("Wrong values to init the gyroscope in ICM-42670P.\n");
+        };
+        ICM42670_enable_accel_gyro_ln_mode();
+    } else {
+        printf("Failed to initialize ICM-42670P.\n");
+    }
+
+        
+
+
+
+    // Create tasks
+    
+    // xTaskCreate(sw2_task, "SW2Task", 256, NULL, 1, NULL);
+    // xTaskCreate(rgb_task, "RGBTask", 256, NULL, 1, NULL);
+    // xTaskCreate(buzzer_task, "BuzzerTask", 256, NULL, 4, NULL);
+    // xTaskCreate(light_sensor_task, "LightSensorTask", 256, NULL, 3, NULL);
+    // xTaskCreate(ths_task, "THSTask", 256, NULL, 1, NULL);
+    xTaskCreate(imu_task, "IMUTask", 256, NULL, 1, NULL);
+    // xTaskCreate(led_simple_task, "LEDSimpleTask", 64, NULL, 1, NULL);
+    // xTaskCreate(sw1_task, "SW1Task", 64, NULL, 1, NULL);
+    // xTaskCreate(led_task, "LEDTask", 64, NULL, 2, NULL);
+ 
+
+    //  xTaskCreate(display_task, "DisplayTask", 256, NULL, 2, NULL);
+    // // xTaskCreate(mic_task, "MicTask", 256, NULL, 1, NULL);
+    
+    // Start the FreeRTOS scheduler
+    vTaskStartScheduler();
+
+    return 0;
+}
