c8526bea62e9ff01e7c0432c1c06285ff8ba5619
quynhanh0107 <qanhhle010706@gmail.com>
2025-11-14 10:14:55 +0200

    Enhance state management and USB handling

--- src/main.c
+++ src/main.c
@@ -2,11 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
+
 #include <pico/stdlib.h>
 
 #include <FreeRTOS.h>
 #include <queue.h>
 #include <task.h>
+#include "tusb.h"
 
 #include "tkjhat/sdk.h"
 
@@ -29,27 +31,36 @@ char recv_buffer[BUFFER_SIZE];
 int  recv_index = 0;
 
 // Introducing state
-enum state {IDLE=1, SEND};
+enum state {IDLE=1, SEND, RECEIVE, UPDATE};
 
 // Global state variable, initialized to waiting state
 enum state myState = IDLE;
-
+volatile bool imu_flag = true;
+bool allow_input = true;
 
 // Button task to go in send state
 // Need an interrupt in main
 void button_callback(uint gpio, uint32_t events) {
     // Button 1 pressed: send the message
-    
+    static int space_count = 0;
     if (gpio == BUTTON_SW1) {
-        printf("hi");
         if (myState == IDLE) {
-            myState = SEND;
+            myState = RECEIVE;
+            allow_input = true;
     }
     } else if (gpio == BUTTON_SW2) {
-        printf("hello");
-        if (buffer_index < BUFFER_SIZE - 1) {
+        if (imu_flag) {
             send_buffer[buffer_index++] = ' ';
-    }
+            printf("space\n");
+        }
+        space_count++;
+        if (space_count >= 3) {
+            imu_flag = false;
+            space_count = 0;
+            if (myState == IDLE) {
+                myState = SEND;
+            }
+        }    
     }
     
 }
@@ -68,8 +79,8 @@ void commTask(void *pvParameters) {
             for (int i = 0; i < sizeof(send_buffer); i++) {
                 printf("%c",send_buffer[i]);
             }
-            
-            // maybe a \n at the end of msg
+            write_text(send_buffer);
+            vTaskDelay(pdMS_TO_TICKS(10000));
 
             // Clear buffer
             memset(send_buffer, 0, sizeof(send_buffer[0])*BUFFER_SIZE);
@@ -77,10 +88,33 @@ void commTask(void *pvParameters) {
 
             // then change the state back to idle
             myState = IDLE;
+            clear_display();
+
+        } else if (myState == RECEIVE  && allow_input) {
+            printf("Type your message: ");
+            fflush(stdout);
+            if (fgets(recv_buffer, sizeof(recv_buffer), stdin)) {
+                recv_buffer[strcspn(recv_buffer, "\n")] = '\0';
+                printf("something");
+                fflush(stdout);
+                if (strlen(recv_buffer) > 0) {
+                    printf("Message typed: %s\n", recv_buffer);
+
+                    write_text(recv_buffer);
+                    allow_input = false;
+                } else {
+                    printf("Empty message.\n");
+                }
+            }
+            myState = IDLE;
+            printf("End message");
+            clear_display();
         }
 
         vTaskDelay(pdMS_TO_TICKS(100));
     }
+
+    
 }
 
 
@@ -98,41 +132,76 @@ void imu_task(void *pvParameters) {
         printf("Failed to initialize ICM-42670P.\n");
     }
     
-    //init_red_led();
+    init_buzzer();
+    init_red_led();
     
     // Start collection data here. Infinite loop.
     while (1)
     {
-        if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) == 0) {
-            if (buffer_index < (BUFFER_SIZE-1)) {
-                if (az > FLAT_THRESHOLD) {
-                    printf("dot");
-                    send_buffer[buffer_index++] = '.';
+        if (imu_flag) {
+            if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) == 0) {
+                if (buffer_index < (BUFFER_SIZE-3)) {
+                    if (az > FLAT_THRESHOLD) {
+                        printf("dot\n");
+                        send_buffer[buffer_index++] = '.';
+                        blink_red_led(1);
+                        buzzer_play_tone(440,500);
+                        vTaskDelay(pdMS_TO_TICKS(10000));
+                    }
+                    //DASH: rotating 90 degrees
+                    else if (ay > TILT_THRESHOLD) {
+                        printf("dash\n");
+                        send_buffer[buffer_index++] = '-';
+                        blink_red_led(2);
+                        buzzer_play_tone(800,200);
+                        vTaskDelay(pdMS_TO_TICKS(10000));
+                    }
                 }
-                //DASH: rotating 90 degrees
-                else if (ay > TILT_THRESHOLD) {
-                    printf("dash");
-                    send_buffer[buffer_index++] = '-';
+                else {
+                    printf("buffer is full\n");
                 }
-            }
-            else {
-                printf("buffer is full\n");
-            }
             
-            printf("Accel: X=%.2f, Y=%.2f, Z=%.2f | Gyro: X=%.2f, Y=%.2f, Z=%.2f \n", ax, ay, az, gx, gy, gz);
+                // printf("Accel: X=%.2f, Y=%.2f, Z=%.2f | Gyro: X=%.2f, Y=%.2f, Z=%.2f \n", ax, ay, az, gx, gy, gz);
 
-        } else {
-            printf("Failed to read imu data\n");
+            } else {
+                printf("Failed to read imu data\n");
+            }
+            vTaskDelay(pdMS_TO_TICKS(400));
         }
-        vTaskDelay(pdMS_TO_TICKS(400));
+        
     }
 
+}
+
+static void usbTask(void *arg) {
+    (void)arg;
+    while (1) {
+        tud_task();   // handle USB stack events
+    }
+}
+
+void tud_cdc_rx_cb(uint8_t itf) {
+    uint8_t buf[BUFFER_SIZE + 1];
+    uint32_t count = tud_cdc_n_read(itf, buf, sizeof(buf) - 1);
+
+    // Always read from both interfaces to avoid stalling CDC0
+    if (itf == 0) tud_cdc_read_flush();  
+
+    // Null-terminate to make it a string
+    buf[count] = '\0';
+
+    // Show on the display
+    write_text((char *)buf);
+
+    // Send acknowledgment back to host
+    tud_cdc_n_write(itf, (uint8_t const *)"OK\n", 3);
+    tud_cdc_n_write_flush(itf);
 }
 
 // not needed until receiving
 // Handling display update
 /*void displayTask(void *pvParameters) {
-
+    init_display();
     while (1) {
     
         if (myState == UPDATE) {
@@ -144,7 +213,7 @@ void imu_task(void *pvParameters) {
             myState = IDLE;
         }
     
-        vTaskDelay(..);
+        vTaskDelay(400);
     }
 }*/
 
@@ -154,11 +223,14 @@ int main() {
     init_hat_sdk();
     init_sw1();
     init_sw2();
+    init_display();
     while (!stdio_usb_connected()){
         sleep_ms(10);
     } 
     printf("Start tests\n");
-        
+    /*write_text("Start...");
+    vTaskDelay(400);
+    clear_display();*/
     // Setup buttons (already initialized in SDK, we only attach callbacks)
     gpio_set_irq_enabled_with_callback(BUTTON_SW1, GPIO_IRQ_EDGE_FALL, true, button_callback);
     gpio_set_irq_enabled(BUTTON_SW2, GPIO_IRQ_EDGE_FALL, true);
@@ -166,7 +238,14 @@ int main() {
     // Create tasks
     xTaskCreate(imu_task, "IMUTask", 256, NULL, 1, NULL);
     xTaskCreate(commTask, "CommTask", 512, NULL, 2, NULL);
-    
+    TaskHandle_t hUsb = NULL;
+    xTaskCreate(usbTask, "usb", 1024, NULL, 3, &hUsb);
+    //xTaskCreate(displayTask, "displayTask", 512, NULL, 2, NULL);
+    #if (configNUMBER_OF_CORES > 1)
+        vTaskCoreAffinitySet(hUsb, 1u << 0);
+    #endif
+    tusb_init();
+    //usb_serial_init();
     // Start the FreeRTOS scheduler
     vTaskStartScheduler();
 
