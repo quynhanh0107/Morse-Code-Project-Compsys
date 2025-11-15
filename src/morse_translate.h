b8514ee6e63cb20523b726684de0340b399399ba
quynhanh0107 <qanhhle010706@gmail.com>
2025-11-15 14:58:54 +0200

    Declare extern for morse_table and MORSE_TABLE_SIZE

--- src/morse_translate.h
+++ src/morse_translate.h
@@ -16,16 +16,8 @@ typedef struct {
   char *morse;
 } morse_table_t;
 
-const morse_table_t morse_table[] = {
-    {'A', ".-"},    {'B', "-..."},  {'C', "-.-."},  {'D', "-.."},
-    {'E', "."},     {'F', "..-."},  {'G', "--."},   {'H', "...."},
-    {'I', ".."},    {'J', ".---"},  {'K', "-.-"},   {'L', ".-.."},
-    {'M', "--"},    {'N', "-."},    {'O', "---"},   {'P', ".--."},
-    {'Q', "--.-"},  {'R', ".-."},   {'S', "..."},   {'T', "-"},
-    {'U', "..-"},   {'V', "...-"},  {'W', ".--"},   {'X', "-..-"},
-    {'Y', "-.--"},  {'Z', "--.."},  {'1', ".----"}, {'2', "..---"},
-    {'3', "...--"}, {'4', "....-"}, {'5', "....."}, {'6', "-...."},
-    {'7', "--..."}, {'8', "---.."}, {'9', "----."}, {'0', "-----"}};
+extern const morse_table_t morse_table[];
+extern const size_t MORSE_TABLE_SIZE;
 
 char morse_to_ascii(const char *morse);
 void decode_morse_message(const char *morse, char *output);
